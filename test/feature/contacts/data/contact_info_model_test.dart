ff9c76db3b9ccd7ee715983da271fddc3fb0fd3a
Abraham Morillo <abrahammorillo777@gmail.com>
2021-12-11 10:53:56 -0400

    [ADD] json and methods to test model

--- test/feature/contacts/data/contact_info_model_test.dart
+++ test/feature/contacts/data/contact_info_model_test.dart
@@ -1,12 +1,47 @@
+import 'dart:convert';
+
 import 'package:contactsapp/feature/contacts/data/models/contact_info_model.dart';
 import 'package:contactsapp/feature/contacts/domain/entities/contact_info.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  var contactModel =
-      const ContactInfoModel(id: 1, name: '', phoneNumber: '', address: '');
+import '../../../core/fixture/fixture.dart';
 
-  test('Test model are equal to entities', () {
+void main() {
+  const contactModel = ContactInfoModel(
+      id: 1,
+      name: 'Abraham',
+      phoneNumber: '809-532-5315',
+      address: 'Bella Vista');
+
+  test('Test model is a subclass of entities', () {
+    //assert
     expect(contactModel, isA<ContactInfo>());
+  });
+
+  group('Contact info dbmap', () {
+    test('Should return same structure like model', () {
+      //arrange
+      final Map<String, dynamic> dbMap =
+          jsonDecode(fixture('contact_db_map.json'));
+      //act
+      final result = ContactInfoModel.fromDbMap(dbMap);
+      //assert
+      expect(result, contactModel);
+    });
+  });
+
+  group('Contact info todbmap', () {
+    test('Should encode json', () {
+      //arrange
+      var jsonEncoded = {
+        "name": "Abraham",
+        "phoneNumber": "809-532-5315",
+        "address": "Bella Vista"
+      };
+      //act
+      final result = contactModel.toDbMap();
+      //assert
+      expect(result, jsonEncoded);
+    });
   });
 }
